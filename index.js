bb1e82ea5f666e34059211796e50982309d10d4a
BenjaminBPJ <pierrejeanbenjamin@hotmail.fr>
2021-07-26 22:58:23 +0200

    creation du burger menu

---
+++ index.js
@@ -0,0 +1,17 @@
+let toogleNavStatus = false;
+
+getBurgerMenu = () => {
+    let toogle = document.querySelector('.bar-menu');
+    let nav = document.querySelector('.nav-header')
+    toogle.addEventListener('click', () => {
+        if (toogleNavStatus === false) {
+            nav.classList.remove('hidden')
+            return toogleNavStatus = true
+        } else if (toogleNavStatus === true) {
+            nav.classList.add('hidden')
+            return toogleNavStatus = false
+        }
+    })
+}
+
+getBurgerMenu()
